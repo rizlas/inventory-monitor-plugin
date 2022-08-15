ad2a024e2d2a98f09de1876bf3ad648d09cf01cf
root <root@krupa.vm.cesnet.cz>
2022-08-15 13:15:20 +0000

    Add detail view for Probe

--- inventory_monitor/views.py
+++ inventory_monitor/views.py
@@ -1,8 +1,18 @@
 from netbox.views import generic
 from . import forms, models, tables
 
+
 class ProbeView(generic.ObjectView):
     queryset = models.Probe.objects.all()
+
+    def get_extra_context(self, request, instance):
+        table = tables.ProbeTable(
+            models.Probe.objects.filter(serial=instance.serial))
+        table.configure(request)
+
+        return {
+            'probe_table': table,
+        }
 
 
 class ProbeListView(generic.ObjectListView):
