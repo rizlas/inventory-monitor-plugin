ef7ad6caede686b4b68e7fa32f62be49cddb7728
Jan Krupa <jan.krupa@cesnet.cz>
2022-08-22 11:06:07 +0000

    Display probed inventory in device detail

---
+++ inventory_monitor/template_content.py
@@ -0,0 +1,36 @@
+from extras.plugins import PluginTemplateExtension
+from .models import Probe
+from django.db.models import OuterRef, Subquery, Count
+
+#from django.conf import settings
+#plugin_settings = settings.PLUGINS_CONFIG.get('inventory_monitor', {})
+
+
+class DeviceDocumentList(PluginTemplateExtension):
+    model = 'dcim.device'
+
+    def right_page(self):
+        obj = self.context['object']
+        latest_inventory_pks = Probe.objects.all()\
+            .distinct('serial')\
+            .order_by('serial', '-time')\
+            .values('pk')
+
+        sub_count_serial = Probe.objects.filter(serial=OuterRef('serial'))\
+            .values('serial')\
+            .annotate(changes_count=Count('*'))
+
+        device_probes = Probe.objects.filter(device=obj, pk__in=latest_inventory_pks)\
+            .prefetch_related('tags', 'device')\
+            .annotate(changes_count=Subquery(sub_count_serial.values("changes_count")))
+
+        return self.render(
+            'inventory_monitor/device_probes_include.html',
+            extra_context={
+                'device_probes': device_probes.order_by('-time')[:10],
+                'total_device_probes_count': device_probes.count(),
+            }
+        )
+
+
+template_extensions = [DeviceDocumentList]
