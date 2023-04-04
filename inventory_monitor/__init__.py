7746e177175c3dbe30102b1c85d79a74767dd0ec
Jan Krupa <jan.krupa@cesnet.cz>
2023-04-04 11:54:24 +0000

    Allow nullable device reference

--- inventory_monitor/__init__.py
+++ inventory_monitor/__init__.py
@@ -5,7 +5,7 @@ class NetBoxInventoryMonitorConfig(PluginConfig):
     name = 'inventory_monitor'
     verbose_name = ' Inventory Monitor'
     description = 'Manage inventory discovered by SNMP'
-    version = '4.0.1'
+    version = '4.0.2'
     base_url = 'inventory-monitor'
 
 config = NetBoxInventoryMonitorConfig
