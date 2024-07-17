fa2fd9a7008cad763f75a25f4c47635bfa1caf5b
Jan Krupa <jan.krupa@cesnet.cz>
2024-07-17 07:43:03 +0000

    Refactor attachments_count in views

--- inventory_monitor/views/invoice.py
+++ inventory_monitor/views/invoice.py
@@ -5,24 +5,29 @@ from netbox.views import generic
 from inventory_monitor import filtersets, forms, models, tables
 from inventory_monitor.helpers import get_object_type_or_none
 
+# Attempt to import NetBoxAttachment and set a flag based on its availability
 try:
     from netbox_attachments.models import NetBoxAttachment
 
     attachments_model_exists = True
-except ModuleNotFoundError:
+except (ModuleNotFoundError, Exception):
     attachments_model_exists = False
 
 
-class InvoiceView(generic.ObjectView):
-    queryset = models.Invoice.objects.all()
-
-
-class InvoiceListView(generic.ObjectListView):
+def get_invoice_queryset():
+    """
+    Generates the queryset for Invoice objects, optionally annotating with attachment counts.
+    """
+    base_queryset = models.Invoice.objects.all()
     if attachments_model_exists:
         try:
             invoice_object_type = get_object_type_or_none(
                 app_label="inventory_monitor", model="invoice"
             )
+
+            if not invoice_object_type:
+                return base_queryset.annotate(attachments_count=Value(0))
+
             subquery_attachments_count = (
                 NetBoxAttachment.objects.filter(
                     object_id=OuterRef("id"), object_type=invoice_object_type
@@ -30,19 +35,25 @@ class InvoiceListView(generic.ObjectListView):
                 .values("object_id")
                 .annotate(attachments_count=Count("*"))
             )
-            queryset = models.Invoice.objects.all().annotate(
+            return base_queryset.annotate(
                 attachments_count=Subquery(
                     subquery_attachments_count.values("attachments_count")
                 )
             )
-        except (ObjectType.DoesNotExist, ValueError) as e:
-            queryset = models.Invoice.objects.all().annotate(attachments_count=Value(0))
-    else:
-        queryset = models.Invoice.objects.all().annotate(attachments_count=Value(0))
+        except (ObjectType.DoesNotExist, ValueError):
+            pass
+    return base_queryset.annotate(attachments_count=Value(0))
 
+
+class InvoiceView(generic.ObjectView):
+    queryset = models.Invoice.objects.all()
+
+
+class InvoiceListView(generic.ObjectListView):
     filterset = filtersets.InvoiceFilterSet
     filterset_form = forms.InvoiceFilterForm
     table = tables.InvoiceTable
+    queryset = get_invoice_queryset()
 
 
 class InvoiceEditView(generic.ObjectEditView):
