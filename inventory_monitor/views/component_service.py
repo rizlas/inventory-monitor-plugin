44113ca8102e1cd567b689a5ad14f41b353054a0
Jan Krupa <jan.krupa@cesnet.cz>
2024-11-05 08:23:28 +0000

    Move ProbeDiffView to proper file

--- inventory_monitor/views/component_service.py
+++ inventory_monitor/views/component_service.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.views.generic import View
 from netbox.views import generic
 
 from inventory_monitor import filtersets, forms, models, tables
@@ -23,49 +21,3 @@ class ComponentServiceEditView(generic.ObjectEditView):
 
 class ComponentServiceDeleteView(generic.ObjectDeleteView):
     queryset = models.ComponentService.objects.all()
-
-
-class ProbeDiffView(View):
-    def post(self, request):
-        # load from, to and device_id from request
-        date_from = request.POST.get("date_from")
-        date_to = request.POST.get("date_to")
-        device_id = request.POST.get("device")
-
-        probes_added = models.Probe.objects.filter(
-            device_id=device_id,
-            creation_time__gte=date_from,
-            creation_time__lte=date_to,
-        )
-        probes_removed = models.Probe.objects.filter(
-            device_id=device_id, time__gte=date_from, time__lte=date_to
-        )
-
-        form = forms.ProbeDiffForm(
-            initial={
-                "date_from": date_from,
-                "date_to": date_to,
-                "device": device_id,
-            }
-        )
-
-        return render(
-            request,
-            "./inventory_monitor/probe_diff.html",
-            {
-                "probes_added": probes_added,
-                "probes_removed": probes_removed,
-                "form": form,
-            },
-        )
-
-    def get(self, request):
-        form = forms.ProbeDiffForm()
-
-        return render(
-            request,
-            "./inventory_monitor/probe_diff.html",
-            {
-                "form": form,
-            },
-        )
